f6e7b3b509773b18a44f17819a4eeabcf9abf089
biletskadi <61203571+biletskadi@users.noreply.github.com>
2020-02-25 20:37:12 +0200

    Update twitter2.py

--- twitter2.py
+++ twitter2.py
@@ -1,10 +1,7 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
 import json
 import ssl
-import urllib.error
-import urllib.parse
-import urllib.request
-
-import twurl
 
 # https://apps.twitter.com/
 # Create App and get the four strings, put them in hidden.py
@@ -16,19 +13,12 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-
 def twit(user):
-    while True:
-        acct = user
-        if len(acct) < 1:
-            break
-        url = twurl.augment(TWITTER_URL,
-                            {'screen_name': acct, 'count': '100'})
-        print('Retrieving', url)
-        connection = urllib.request.urlopen(url, context=ctx)
-        data = connection.read().decode()
-        js = json.loads(data)
-        with open('twit.json', 'w') as f:
-            json.dump(js, f, ensure_ascii=False, indent=4)
-            y = json.dumps(js, indent=2)
-        return y
+    acct = user
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': acct, 'count': '100'})
+    print('Retrieving', url)
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+    js = json.loads(data)
+    return js
